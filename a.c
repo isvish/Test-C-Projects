6a9e48f8e766988e39713db6728b8e12ad0dd760
AM <abhishek.mishra2k16@gmail.com>
2019-10-11 23:22:00 +0000

    test build trigger second time

--- a.c
+++ a.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main() {
-	printf("\n Hello Jenkins to trigger builds\n\n");
+  printf("\n Hello Jenkins to trigger builds\n\n");
+  printf("HELLO JENKINS\n");
 return 0;
 }
 
